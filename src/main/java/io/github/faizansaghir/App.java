deb6c7fb5e49a26d55c865cf114d783cb720b417
faizansaghir <dbz.rashidsaghir@gmail.com>
2024-12-27 23:11:24 +0530

    Removed unused imports

--- src/main/java/io/github/faizansaghir/App.java
+++ src/main/java/io/github/faizansaghir/App.java
@@ -4,9 +4,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
-
-import java.io.File;
 
 /**
  * Hello world!
